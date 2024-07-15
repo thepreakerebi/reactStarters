19386ddb198652dfec8bc4fdefc5c46b2cf86f5b
Elvis Kerebi <elviskerebi@Elviss-MacBook-Pro.local>
2024-07-15 13:43:59 +0200

    First project on accordions

---
+++ react-projs/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Accordion from './components/accordion';
+
+function App() {
+  return (
+    <div className="App">
+      {/* This is the accordion */}
+      <Accordion />
+    </div>
+  );
+}
+
+export default App;
